5782ec7367711dae1bbdbdecf86b62c952edba90
thomaslombart <lombart.thomas@icloud.com>
2021-08-01 18:56:56 +0200

    Remove previous service workers

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 
 import "../styles/global.css";
 import "../styles/fonts.css";
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
